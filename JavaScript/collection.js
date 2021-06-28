dd8cad0577459fc78e5406a1e1ac67b22e9bf53e
samofoke <samofoke@student.wethinkcode.co.za>
2021-06-28 21:38:52 +0200

    basics in javascript

---
+++ JavaScript/collection.js
@@ -0,0 +1,29 @@
+let books = {
+    //this is a json object wich allows one to access
+    //the contents inside.
+    topic: "lord of the flies",
+
+    author: "Helmet"
+};
+
+books.topic
+books["year"]
+books.price = 100;
+
+console.log(books.topic);
+console.log(books);
+
+
+//looking at numbers.
+let p = [2, 4, 6, 8, 10];
+console.log(p.length);
+console.log(p.length - 1);
+
+//looking at an array with 2 elements.
+let cd = [
+    {s: 3, a: 3},
+    {s: 2, a: 2}
+];
+
+console.log(cd[0]);
+
