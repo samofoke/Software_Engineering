3f069f37fca48dcd7aa4e2a05bd8bd37bf849348
samofoke <samofoke@student.wethinkcode.co.za>
2021-07-10 18:43:56 +0200

    add fuction working

--- training-app/src/components/add/add-contact.js
+++ training-app/src/components/add/add-contact.js
@@ -6,24 +6,43 @@ class AddContact extends React.Component {
         name: "",
         email: ""
     };
+
+    add = (event) => {
+        event.preventDefault();
+
+        if (this.state.name === "" && this.state.email === "") {
+            alert("you need to fill all thefields.");
+            return;
+        }
+        console.log(this.state);
+    }
     
     render() {
         return (
             <div className="ui main">
                 <h2>Add contact</h2>
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
                         <input 
                             type="text"
                             name="name" 
                             placeholder="name"
+                            value={this.state.name}
+                            onChange={(event)=> this.setState({ name: event.target.value})}
                         >
                         </input>
                     </div>
                     <div className="field">
                         <label>Email</label>
-                        <input type="text" name="email" placeholder="email"></input>
+                        <input
+                            type="text"
+                            name="email"
+                            placeholder="email"
+                            value={this.state.email}
+                            onChange={(event)=> this.setState({ email: event.target.value})}
+                        >
+                        </input>
                     </div>
                     <button className="ui button red">Add</button>
                 </form>
