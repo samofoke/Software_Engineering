ac74abe9edbdc987be517ec3c260c732f695a164
samofoke <samofoke@student.wethinkcode.co.za>
2021-11-06 18:05:09 +0200

    going through destructuring assignments in js

--- JavaScript/repeated_declearations.js
+++ JavaScript/repeated_declearations.js
@@ -1,5 +1,5 @@
 //we look at the syntax in JS
-//
+
 const x = 1;
 
 if (x === 1) {
@@ -13,4 +13,52 @@ console.log("outside the block of code (global scope)", x);
  * same variable multiple times with var. Also var variables 
  * have function scope instead of block scope,
  */
+
+/* Destructuring Assignment
+ * In a destructuring assignment, the value on the righthand side of 
+ * the equals sign is an array or object (a “structured” value), and 
+ * the lefthand side specifies one or more variable names using a syntax 
+ * that mimics array and object literal syntax.
+ */
+
+let [a1, b2] = [2, 4];
+/*this is saying let x = 2 and y = 4, This is an example of destructuring
+ * assignment that makes it easy to work with function.
+ */
+function get_coordinates(a, b) {
+    return([Math.sqrt(a*a+b*b), Math.atan2(b, a)]);
+}
+
+console.log(get_coordinates(2, 4));
+
+/* convert the polar to cartesian
+ */
+
+function convertCartesian(r, tr) {
+    return([r*Math.cos(tr), r*Math.sin(tr)]);
+}
+
+/*going to pass this function to get_coordinates
+ */
+
+let [r, tr] = get_coordinates(1.1, 4.4);
+
+console.log(convertCartesian(r, tr));
+/* On this line I am printing out what I ahve destructured on get_coordinates.
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
