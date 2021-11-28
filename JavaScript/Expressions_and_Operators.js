e6142bfe27194b1a100d2eb543a44b0e3daa1708
samofoke <samofoke@student.wethinkcode.co.za>
2021-11-28 16:21:09 +0200

    Invocation Expressions]

--- JavaScript/Expressions_and_Operators.js
+++ JavaScript/Expressions_and_Operators.js
@@ -70,6 +70,61 @@ console.log(ngl);
  * let arr = ["a", 3, [3, 4]];
  */
 
+/*The .identifier syntax is the simpler of the two property access options.
+ * let ob = {x: 1, y: 2} if I console ob it will print out an Object but
+ * if I use the .identifier like ob.x I will get the value of x which is
+ * 1 in this case. for an example for an Array let arr = ["a", 3, [3, 4]];
+ * arr[0] which console 'a' and arr[2][1] which results to 4.
+ */
+
+/*In JavaScript, the values null and undefined are the only two values 
+ * that do not have properties. This form of property access expression
+ * is sometimes called “optional chaining” because it also works for longer
+ * “chained” property access expressions like this one:
+ * let a = { b: null };
+ * a.b = null
+ * a.b?.c.d = undefined
+ * a is an object, so a.b is a valid property access expression. But the
+ * value of a.b is null, so a.b.c would throw a TypeError. By using?. 
+ * instead of . we avoid the TypeError.
+ */
+
+/*Invocation Expressions
+ * An invocation expression is JavaScript’s syntax for calling (orexecuting) 
+ * a function or method. It starts with a function expression that identifies
+ * the function to be called.
+ */
+
+/* Conditional Invocation
+ * you can invoke a function by using ? instead of using (). If the expression
+ * in the left of the parentheses is null or undefined or any other 
+ * non-function, a TypeError is thrown. But with the new evaluation it wont
+ * throw an error.
+ */
+
+//Then example.
+function sqrt(s, log) {
+    if (log) {
+        log(s);
+    }
+    return s * s;
+}
+
+//conditional invacation syntax in 2020.
+function sqrt2(v, logx) {
+    logx?.(v);//this checks whether the lefthand side is null or undefined.
+    //it does not verify that the value is actually a function.
+    return v * v;
+}
+
+
+
+
+
+
+
+
+
 
 
 
