e403b94e43c94970bdd092f33ccd328aab60bd1f
samofoke <samofoke@student.wethinkcode.co.za>
2022-01-24 13:45:26 +0200

    going through statements

---
+++ JavaScript/statements.js
@@ -0,0 +1,33 @@
+/*Statements are JavaScript sentences or commands
+ */
+
+/*Conditionals
+ * They're like an if or switch statements the JS interpreter execute.
+ * -> Loops 
+ *  Statements such as a while and a for.
+ *  -> Jumps
+ *  Statements like break, return, and throw that cause the interpreter to 
+ *  jump.
+ */
+
+/*The primtive statements withing the block end in semicolons, but the block it
+ * self does not.
+ * {
+ *      console.log("cos = ", x);
+ * }
+ */
+/*Looking at the realistic example for switch statement
+ * 
+ * function pass(x) {
+    switch(typeof x) {
+        case "number":
+            return x.toString(10);
+        case "string":
+            return "''" + x + "''";
+        default:
+            return String(x);
+        }
+    }
+    console.log(pass(5));
+    */
+
