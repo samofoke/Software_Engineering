9f3860c38557c9de3e07a13015dd76f97d3b386d
samofoke <sabataernestmofokeng@gmail.com>
2022-03-16 19:23:34 +0200

    building basic routes

--- react_software_architecture/src/App.js
+++ react_software_architecture/src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import About from './pages/about';
+import Home from './pages/Home';
+import Postpage from './pages/post';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+        <h1>Building a Server side sample</h1>
+        <ul>
+          <li>
+            <Link to="/" >Home</Link>
+          </li>
+          <li>
+            <Link to="/about" >About</Link>
+          </li>
+          <li>
+            <Link to="/post" >Posts</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about" >
+            <About />
+          </Route>
+          <Route>
+            <Postpage path="/post" />
+          </Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
