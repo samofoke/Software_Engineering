cdc89982c338523c22c078c797289768dcd11f3e
samofoke <sabataernestmofokeng@gmail.com>
2022-08-08 16:16:15 +0200

    looking at floatinng numbers

--- go_training/numbers.go
+++ go_training/numbers.go
@@ -7,11 +7,20 @@ import (
 )
 
 func main() {
-    var x int
+   /* var x int
     var y int
 
     x = 2
     y = 4
+    */
+
+    //In go we have float32 and float64 bit
+
+    var x float64
+    var y float64
+
+    x = 1.0
+    y = 2.0
 
     fmt.Printf("x = %v, type of %T\n", x, x)
     fmt.Printf("y = %v, type of %T\n", y, y)
