dd4e585de6dba3e0376ce25c025e83ca51738d90
samofoke <sabataernestmofokeng@gmail.com>
2022-08-22 05:01:10 +0200

    doing training on C++

---
+++ cpp_computing/c++_essential_training.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+/* The Develpoment of C++ cycle
+ * we have source code, preprocessor, compiler, linker and run.
+ */
+
+/*int main()
+{
+    puts("I am runnning and the is no essues.");
+    //cout::std << "Well is it working";
+    printf("Hello worrld!!");
+    return(0);
+}
+*/
+
+/*When using the cstdio standard library cout::std is not supported.
+ * it also support modern c++ compilers.
+ */
+
