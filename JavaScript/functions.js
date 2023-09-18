5777472b44f7e53eb20ad1df5fe3ba0bd51c98d2
samofoke <sabataernestmofokeng@gmail.com>
2023-09-18 03:25:03 +0200

    looking at memoization

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -38,3 +38,71 @@ const odd = not(even);
 data.every(odd);
 
 console.log("this is odd: ", odd);
+
+
+// Memoization
+//  This is caching or storing the results of a function.
+
+function memoize(f) {
+
+    const cache = new Map()
+
+    return function(...args) {
+        let key = args.length + args.join("+");
+        if (cache.has(key)) {
+            return cache.get(key);
+        } else {
+            let result = f.apply(this, args);
+            cache.set(key, result);
+            return result;
+        }
+    }
+}
+
+// using this function with an example of greatest commonn divisor.
+
+function gcd(a, b) {
+    if (a < b) {
+        [a, b] = [b, a];
+    }
+    while (b !== 0) {
+        [a, b] = [b, a % b];
+    }
+    return a;
+}
+
+const gcdmemo = memoize(gcd);
+
+console.log("The gcd memoize: ",gcdmemo(85, 187));
+
+//writing the function recursive.
+
+const factorial = memoize(function(n) {
+    return (n <= 1) ? 1 : n * factorial(n - 1);
+})
+
+console.log("using memoize in factorial: ", factorial(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
