16b19e88d64c3a751367d8afe80b978212f2e517
samofoke <sabataernestmofokeng@gmail.com>
2023-09-19 21:06:15 +0200

    looking at prototypes

--- JavaScript/classes.js
+++ JavaScript/classes.js
@@ -42,6 +42,49 @@ console.log("operator: ", [...r]);
 // A Constructor is a function designed for the initialization of newly
 // created objects.
 
+// The prototype object is a fundamental to the identity of a class.
+// two objects are instances of the same class if and only if they inherit from 
+// same prototype object.
+
+console.log(range.methods.isPrototypeOf(r))
+
+// So in this example range.methods is a prototype object.
+
+// This is the mmodern way of writing Classes in JavaScript.
+
+class Range {
+    constructor(from, to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    includes(x) {
+        return this.from <= x && x <= this.to;
+    }
+
+    *[Symbol.iterator]() {
+        for (let x = Math.ceil(this.from); x <= this.to; x++) {
+            yield x;
+        }
+    }
+
+    toString() {
+        return `(${this.from}...${this.to})`;
+    }
+}
+
+let q = new Range(1, 3)
+
+console.log("the res: ", q);
+console.log("include: ", q.includes(2));
+console.log("to string: ", q.toString());
+console.log("operator: ", [...q]);
+
+
+
+
+
+
 
 
 
