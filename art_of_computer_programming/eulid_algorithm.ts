30342ee64c2cf8e7814826ef26a01decae795f71
samofoke <sabataernestmofokeng@gmail.com>
2025-06-22 16:10:04 +0200

    the implementation of the euclid algorithm

---
+++ art_of_computer_programming/eulid_algorithm.ts
@@ -0,0 +1,34 @@
+/*
+ * This the basic of going htrough algorithms
+ 
+ *----------------------------------------------------------------
+
+ * (Euclid’s algorithm). Given two positive integers m and n, find
+ * their greatest common divisor, that is, the largest positive integer that evenly
+ * divides both m and n.
+ *
+ * The approach I am taking with this using Typescript with an interface to focus
+ * on the shape of the types, this allows us to discribe what our function take 
+ *
+ * We going to wrok out different solution which use different techniques
+ *
+*/
+
+interface IntegerEuclidProps {
+  m: number;
+  n: number;
+}
+
+//This is the first approach to the solution
+const euclidAlgorithm1 = ({ m, n }: IntegerEuclidProps): number => {
+  let temp: number;
+
+  if (n !== 0) {
+    temp = m % n;
+    m = n;
+    n = temp;
+  }
+  return m;
+};
+
+console.log(euclidAlgorithm1({ m: 20, n: 2 }));
